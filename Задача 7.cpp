7365b2bde6cb3f5671b21bc58e40ec1e712a87bf
Maria <lebedeva.maria@stud.onu.edu.ua>
2025-03-21 12:55:08 +0200

    Задача 7
    Реализуйте подсчет чисел Фибоначчи.

---
+++ Задача 7.cpp
@@ -0,0 +1,28 @@
+﻿// Задача 7.cpp :	Реалізуйте підрахунок чисел Фібоначчі.
+#include <iostream>
+
+int fibonacci(int n);
+
+int main() {
+    int n;
+    std::cout << "Enter a number: ";
+    std::cin >> n;
+
+    int result = fibonacci(n);
+    std::cout << "Fibonacci number for n = " << n << " is: " << result << std::endl;
+
+    return 0;
+}
+
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    int a = 0, b = 1;
+    for (int i = 2; i <= n; i++) {
+        int temp = a;
+        a = b;
+        b = temp + b;
+    }
+    return b;
+}
