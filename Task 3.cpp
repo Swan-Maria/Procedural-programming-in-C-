4b76fb9e60593e49f784921313b126bb6f52f9dc
Maria <lebedeva.maria@stud.onu.edu.ua>
2025-03-21 12:33:14 +0200

    Рекурсивный алгоритм Евклида
    Для быстрого вычисления наибольшего общего делителя (нод) двух чисел используют алгоритм Евклида. Он построен в следующем соотношении: НСД(a,b)=НСД(b,a % b). Реализуйте рекурсивный алгоритм Евклида как функцию

---
+++ Task 3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+// Функція для обчислення НСД за допомогою алгоритму Евкліда
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+int main() {
+    int a, b;
+    std::cout << "Enter two integers:\na: ";
+    std::cin >> a;
+    std::cout << "b: ";
+    std::cin >> b;
+
+    int result = gcd(a, b);
+    std::cout << "GCD of " << a << " and " << b << " is " << result << std::endl;
+
+    return 0;
+}
