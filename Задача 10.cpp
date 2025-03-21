496d4300019a7538e2bf728e793e53183414a429
Maria <lebedeva.maria@stud.onu.edu.ua>
2025-03-21 12:57:55 +0200

    Задача 10
    Напишите функцию для определения, есть ли определенное слово палиндромом.

---
+++ Задача 10.cpp
@@ -0,0 +1,33 @@
+﻿// Задача 10.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+#include <iostream>
+
+bool isPalindrome(const char* str);
+
+int main() {
+    char str[100];
+    std::cout << "Enter a word: ";
+    std::cin >> str;
+
+    if (isPalindrome(str)) {
+        std::cout << str << " is a palindrome." << std::endl;
+    }
+    else {
+        std::cout << str << " is not a palindrome." << std::endl;
+    }
+
+    return 0;
+}
+
+bool isPalindrome(const char* str) {
+    int len = 0;
+    while (str[len] != '\0') {
+        len++;
+    }
+
+    for (int i = 0; i < len / 2; i++) {
+        if (str[i] != str[len - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
