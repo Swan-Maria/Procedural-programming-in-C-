551883a94e32de49daa37450e0b72f9d949a3763
Maria <lebedeva.maria@stud.onu.edu.ua>
2025-03-21 12:59:01 +0200

    Задача 11
    Напишите функцию для поиска уникального элемента в массиве.

---
+++ Задача 11.cpp
@@ -0,0 +1,33 @@
+﻿// Задача 11.cpp :	Напишіть функцію для пошуку унікального елемента в масиві.
+#include <iostream>
+
+int findUniqueElement(int* arr, int size);
+
+int main() {
+    int arr[] = { 1, 2, 2, 3, 3, 4, 4, 5, 5 };
+
+    int uniqueElement = findUniqueElement(arr, 9);
+    if (uniqueElement != -1) {
+        std::cout << "Unique element: " << uniqueElement << std::endl;
+    }
+    else {
+        std::cout << "No unique element found." << std::endl;
+    }
+
+    return 0;
+}
+int findUniqueElement(int* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        bool isUnique = true;
+        for (int j = 0; j < size; j++) {
+            if (i != j && arr[i] == arr[j]) {
+                isUnique = false;
+                break;
+            }
+        }
+        if (isUnique) {
+            return arr[i];
+        }
+    }
+    return -1;
+}
