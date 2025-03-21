debb8cd8a91c09301205fd84a69ff09da5b859fc
Maria <lebedeva.maria@stud.onu.edu.ua>
2025-03-21 12:39:49 +0200

    Задача 1
    Посчитайте количество единиц в произвольном числе.

---
+++ Задача 1.cpp
@@ -0,0 +1,26 @@
+﻿// Задача 1.cpp : Порахуйте кількість одиниць у довільному числі.
+#include <iostream>
+
+int counter(int n);
+
+int main() {
+    int n;
+    std::cout << " Input number: ";
+    std::cin >> n;
+
+    int ones = counter(n);
+    std::cout << " Number of ones: " << ones << std::endl;
+
+    return 0;
+}
+
+int counter(int n) {
+    int count = 0;
+    while (n != 0) {
+        if (n % 10 == 1) {
+            count++;
+        }
+        n /= 10;
+    }
+    return count;
+}
