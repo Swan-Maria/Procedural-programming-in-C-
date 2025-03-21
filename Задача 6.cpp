1741791afbbecc87f877146bfb85aea788e3a898
Maria <lebedeva.maria@stud.onu.edu.ua>
2025-03-21 12:53:53 +0200

    Задача 6
    Реализуйте подсчет слов в предложении.

---
+++ Задача 6.cpp
@@ -0,0 +1,33 @@
+﻿// Задача 6.cpp : Реалізуйте підрахунок слів у реченні.
+#include <iostream>
+int countWords(const char* str);
+
+int main()
+{
+    const int size = 100;
+    char str[size];
+
+    std::cout << "Enter a string: ";
+    std::cin.getline(str, size); // Чтение строки с консоли
+
+    int wordCount = countWords(str);
+    std::cout << "Number of words: " << wordCount << std::endl;
+
+    return 0;
+}
+
+int countWords(const char* str) {
+    int count = 0;
+    int state = 0;
+    while (*str) {
+        if (*str == ' ' || *str == '\n' || *str == '\t') {
+            state = 0;
+        }
+        else if (state == 0) {
+            state = 1;
+            count++;
+        }
+        str++;
+    }
+    return count;
+}
